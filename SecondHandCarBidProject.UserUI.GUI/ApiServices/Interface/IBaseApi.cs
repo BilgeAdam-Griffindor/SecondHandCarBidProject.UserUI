836c3b8a06f4678f4a46dd6c7c923acbb37be66b
Endalth <alican__karamaras@hotmail.com>
2022-10-04 13:33:32 +0300

    BaseApi ve bootstrap

---
+++ SecondHandCarBidProject.UserUI.GUI/ApiServices/Interface/IBaseApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Interface
+{
+    public interface IBaseApi
+    {
+        Task<TReturn> PostAsync<TReturn, TData>(string url, TData postData);
+        Task<TReturn> PutAsync<TReturn, TData>(string url, TData postData);
+        Task<TReturn> GetByIdAsync<TReturn>(string url, object id);
+        Task<TReturn> DeleteByIdAsync<TReturn>(string url, object id);
+    }
+}
