1213151157d95a5eb419c94b544780aa00abd7aa
emre çakmak <45544939+EmreCkm98@users.noreply.github.com>
2022-10-05 09:25:04 +0300

    Bid List Page added

---
+++ SecondHandCarBidProject.UserUI.GUI/Controllers/BidController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SecondHandCarBidProject.UserUI.GUI.Controllers
+{
+    public class BidController : Controller
+    {
+        // GET: Bid
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BidList()
+        {
+            ViewBag.Liste = new List<string> { "1", "yeni ihale", "bireysel", "05/10/2022", "07/10/2022", "Bitti", "EmreÇ", "2020/05/10-11.00" };
+            return View();
+        }
+    }
+}
