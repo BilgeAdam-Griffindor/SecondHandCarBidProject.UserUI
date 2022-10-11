4efe0192e3f84cdcc9054b00c81626c0d070fa2a
ernkny <eren.kinay.98@windowslive.com>
2022-10-12 02:23:53 +0300

    Logger Added GUI

---
+++ SecondHandCarBidProject.Logging/Concrete/FileLogger.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using SecondHandCarBidProject.Logging.Abstract;
+using SecondHandCarBidProject.Logging.LogModels;
+using SecondHandCarBidProject.Logging.Nlog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.Logging.Concrete
+{
+    public class FileLogger : ILoggerExtension
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public FileLogger()
+        {
+            LogConfig logConfig = new LogConfig();
+        }
+        public Task DataLog(LogModel data)
+        {
+            string message = null;
+            try
+            {
+                message = JsonConvert.SerializeObject(data, Formatting.Indented,
+                   new JsonSerializerSettings
+                   {
+                       PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                   });
+                Logger.Info(message);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Hata Oluştu " + DateTime.Now + " " + message);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
