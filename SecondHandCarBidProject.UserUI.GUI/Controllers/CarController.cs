1bfbf4eeccb4460ec27bd49299195b2a67ac0436
Endalth <alican__karamaras@hotmail.com>
2022-10-12 11:38:36 +0300

    Car/Index page

--- SecondHandCarBidProject.UserUI.GUI/Controllers/CarController.cs
+++ SecondHandCarBidProject.UserUI.GUI/Controllers/CarController.cs
@@ -1,4 +1,6 @@
 ﻿using SecondHandCarBidProject.UserUI.Dto;
+using SecondHandCarBidProject.UserUI.Dto.CarDtos;
+using SecondHandCarBidProject.UserUI.GUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,100 @@ namespace SecondHandCarBidProject.UserUI.GUI.Controllers
 {
     public class CarController : Controller
     {
-        // GET: Car
+        [HttpGet]
+        [ValidateAntiForgeryToken]
         public ActionResult Index()
         {
+            CarListViewModel model = new CarListViewModel();
+            model.BrandList = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text = "Boş",
+                    Value = null
+                },
+                new SelectListItem()
+                {
+                    Text = "Brand 1",
+                    Value = "1"
+                },
+                new SelectListItem()
+                {
+                    Text = "Brand 2",
+                    Value = "2"
+                },
+                new SelectListItem()
+                {
+                    Text = "Brand 3",
+                    Value = "3"
+                }
+            };
+            model.ModelList = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text = "Boş",
+                    Value = null
+                },
+                new SelectListItem()
+                {
+                    Text = "Model 1",
+                    Value = "1"
+                },
+                new SelectListItem()
+                {
+                    Text = "Model 2",
+                    Value = "2"
+                }
+            };
+            model.StatusList = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text = "Boş",
+                    Value = null
+                },
+                new SelectListItem()
+                {
+                    Text = "Status 1",
+                    Value = "1"
+                },
+                new SelectListItem()
+                {
+                    Text = "Status 2",
+                    Value = "2"
+                }
+            };
+            model.TableRows = new List<CarListTableRowDTO>()
+            {
+                new CarListTableRowDTO()
+                {
+                    CarId = Guid.NewGuid(),
+                    BrandName = "Test Brand",
+                    ModelName = "Test Model",
+                    IndividualOrCorporate = "Bireysel",
+                    RegisterDate = DateTime.Now,
+                    Status = "Test Status",
+                    Username = "Test User Name"
+                },
+                new CarListTableRowDTO()
+                {
+                    CarId = Guid.NewGuid(),
+                    BrandName = "Test Brand 2",
+                    ModelName = "Test Model 2",
+                    IndividualOrCorporate = "Bireysel",
+                    RegisterDate = DateTime.Now,
+                    Status = "Test Status 2",
+                    Username = "Test User Name 2"
+                }
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CarListViewModel viewData)
+        {
+
             return View();
         }
 
@@ -25,7 +118,7 @@ namespace SecondHandCarBidProject.UserUI.GUI.Controllers
             return View(carDetailDto);
         }
         [HttpPost]
-        public ActionResult CarDetailInformation(string deneme)
+        public ActionResult CarDetailInformation(CarDetailDto dto)
         {
             return View();
         }
