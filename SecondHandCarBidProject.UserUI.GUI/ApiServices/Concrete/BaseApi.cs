d0c7a2edf2e94eb837b49bdc41d473d8b8b5bfcf
emre çakmak <45544939+EmreCkm98@users.noreply.github.com>
2022-10-13 09:51:45 +0300

    Bid List view styles changed

--- SecondHandCarBidProject.UserUI.GUI/ApiServices/Concrete/BaseApi.cs
+++ SecondHandCarBidProject.UserUI.GUI/ApiServices/Concrete/BaseApi.cs
@@ -1,15 +1,10 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using SecondHandCarBidProject.UserUI.GUI.ApiServices.Interface;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Web;
-using System.Web.UI;
 
 namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Concrete
 {
@@ -23,7 +18,7 @@ namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Concrete
             _client.BaseAddress = new Uri(ConfigurationManager.AppSettings["apiBasePath"]);
         }
 
-        
+
         public async Task<TReturn> LoginAsync<TReturn, TData>(string loginUrl, TData postData)
         {
             var body = new StringContent(JsonConvert.SerializeObject(postData));
@@ -39,12 +34,12 @@ namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Concrete
             return default(TReturn);
         }
 
-        
+
         public async Task<TReturn> PostAsync<TReturn, TData>(string urlSubDirectory, TData postData, string token)
         {
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
             var body = new StringContent(JsonConvert.SerializeObject(postData));
             body.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
 
             var response = await _client.PostAsync(urlSubDirectory, body);
 
@@ -56,12 +51,12 @@ namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Concrete
             return default(TReturn);
         }
 
-        
+
         public async Task<TReturn> PutAsync<TReturn, TData>(string urlSubDirectory, TData putData, string token)
         {
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
             var body = new StringContent(JsonConvert.SerializeObject(putData));
             body.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
 
             var response = await _client.PutAsync(urlSubDirectory, body);
 
@@ -72,26 +67,13 @@ namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Concrete
 
             return default(TReturn);
         }
-        
+
         public async Task<TReturn> GetByFilterAsync<TReturn>(string urlSubDirectory, string token, string filterQueryString = "")
         {
+            if (!string.IsNullOrEmpty(filterQueryString))
+                urlSubDirectory += "?" + filterQueryString;
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
-            string pageQueryString = "page=" + page + "&perPage=" + perPage;
-            var fullQuery = queryString == "" ? pageQueryString : queryString + "&" + pageQueryString;
-
-            var response = await _client.GetAsync(urlSubDirectory + "?" + fullQuery);
-            if (response.IsSuccessStatusCode)
-            {
-                return JsonConvert.DeserializeObject<TReturn>(await response.Content.ReadAsStringAsync());
-            }
-
-            return default(TReturn);
-        }
-
-        public async Task<TReturn> GetByIdAsync<TReturn>(string urlSubDirectory, object id, string token)
-        {
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
-            var response = await _client.GetAsync(urlSubDirectory + "?id=" + id);
+            var response = await _client.GetAsync(urlSubDirectory);
 
             if (response.IsSuccessStatusCode)
             {
@@ -101,11 +83,12 @@ namespace SecondHandCarBidProject.UserUI.GUI.ApiServices.Concrete
             return default(TReturn);
         }
 
-
-        public async Task<TReturn> DeleteByIdAsync<TReturn>(string urlSubDirectory, object id, string token)
+        public async Task<TReturn> DeleteByFilterAsync<TReturn>(string urlSubDirectory, string token, string filterQueryString)
         {
+            if (!string.IsNullOrEmpty(filterQueryString))
+                urlSubDirectory += "?" + filterQueryString;
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
-            var response = await _client.DeleteAsync(urlSubDirectory + "?id=" + id);
+            var response = await _client.DeleteAsync(urlSubDirectory);
 
             if (response.IsSuccessStatusCode)
             {
