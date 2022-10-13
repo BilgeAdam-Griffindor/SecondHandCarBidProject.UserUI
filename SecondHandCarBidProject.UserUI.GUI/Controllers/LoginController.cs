4f7a3da47e6805b04ed85812024c706e12873be0
ernkny <eren.kinay.98@windowslive.com>
2022-10-14 01:08:47 +0300

    Login And Register Added

---
+++ SecondHandCarBidProject.UserUI.GUI/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecondHandCarBidProject.UserUI.GUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
