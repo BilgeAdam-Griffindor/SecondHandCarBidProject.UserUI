650b92b44d643328781726721333e38a07b3062c
Recai-Capkin <recai.capkin28@gmail.com>
2022-10-14 12:40:53 +0300

    Car listesi yapıldı. Ekleme sayfası işlemleri çalışılıyor.

--- SecondHandCarBidProject.UserUI.GUI/TemplateFiles/js/main.js
+++ SecondHandCarBidProject.UserUI.GUI/TemplateFiles/js/main.js
@@ -173,17 +173,17 @@
         max: maxPrice,
         values: [minPrice, maxPrice],
         slide: function (event, ui) {
-            minamount.val('$' + ui.values[0]);
-            maxamount.val('$' + ui.values[1]);
+            minamount.val( ui.values[0]);
+            maxamount.val(ui.values[1]);
         }
     });
-    minamount.val('$' + rangeSlider.slider("values", 0));
-    maxamount.val('$' + rangeSlider.slider("values", 1));
+    minamount.val(rangeSlider.slider("values", 0));
+    maxamount.val(rangeSlider.slider("values", 1));
 
     /*--------------------------
         Select
     ----------------------------*/
-    $("select").niceSelect();
+    /*$("select").niceSelect();*/
 
     /*------------------
 		Single Product
