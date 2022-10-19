b8ae74a9a9a8f396f2a7bfac08d99175576070e3
Essrinc <sabireesraozcan@gmail.com>
2022-10-19 21:45:24 +0300

    advertsave, aboutus and contactus pages

---
+++ SecondHandCarBidProject.UserUI.GUI/Controllers/NavbarController.cs
@@ -0,0 +1,28 @@
+﻿using SecondHandCarBidProject.UserUI.Dto.DTOs;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SecondHandCarBidProject.UserUI.GUI.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Bid
+        //  BaseApi baseApi = new BaseApi();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
